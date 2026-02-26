37cbbab7dd306495c5a7cccf9df0109fb508ea7e
makzilcortespdn-maker <makzil.cortes.pdn@gmail.com>
2026-02-26 17:48:29 -0600

    se agrega ejecucion en 3 horarios al dia

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 # Crear cron job
-echo "00 11 * * * /app/run-reports.sh >> /proc/1/fd/1 2>&1" > /etc/crontabs/root
+# echo "00 11 * * * /app/run-reports.sh >> /proc/1/fd/1 2>&1" > /etc/crontabs/root
+echo "00 11,14,18 * * * /app/run-reports.sh >> /proc/1/fd/1 2>&1" > /etc/crontabs/root
 
 echo "Cron configurado para ejecutarse a las 11:00 AM CDMX"
 echo "Esperando ejecución programada..."
